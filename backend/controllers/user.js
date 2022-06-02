7a7b804be8ef54c0cd3e2c5d1e73b8ec4de10861
Naman <namansngh64@gmail.com>
2022-06-02 18:10:09 +0530

    backend complete

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,61 +1,63 @@
 const User = require("../models/user");
 const formidable = require("formidable");
-const _ = require("lodash")
-const fs = require('fs')
-const { IncomingForm } = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 exports.getUserById = (req, res, next, id) => {
-    User.findById(id).exec((err, user) => {
-        if (err || !user) {
-            return res.json({ error: "No user found!" });
-        }
-        req.profile = user;
-        req.profile.secret = undefined; //hide secret
-        req.profile.encry_password = undefined; //hide encrypted password 
-        next();
-    });
+  User.findById(id).exec((err, user) => {
+    if (err || !user) {
+      return res.json({ error: "No user found!" });
+    }
+    req.profile = user;
+    req.profile.secret = undefined; //hide secret
+    req.profile.encry_password = undefined; //hide encrypted password
+    next();
+  });
 };
 
 exports.updateUser = (req, res) => {
-    let form = new formidable.IncomingForm();
-    form.keepExtension = true;
+  let form = new formidable.IncomingForm();
+  form.keepExtension = true;
 
-    form.parse(req, (err, fields, file) => {
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.json({ error: "Problem with image!" });
+    }
+    const { name, phone, address, userInfo } = fields;
+    if (!name || !phone || !address || !userInfo) {
+      return res.json({ error: "Provide all info" });
+    }
 
+    let user = req.profile;
+    user = _.extend(user, fields); //allows to access extra fields
+    if (file.profile_pic) {
+      if (file.profile_pic.size > 3000000) {
+        return res.json({ error: "File size too big!" });
+      }
+
+      user.profile_pic.data = fs.readFileSync(file.profile_pic.filepath); //adding an attribute
+      user.profile_pic.contentType = file.profile_pic.mimetype;
+    }
+    User.findOneAndUpdate(
+      { _id: req.profile._id },
+      {
+        $set: {
+          name: user.name,
+          address: user.address,
+          phone: user.phone,
+          userInfo: user.userInfo,
+          profile_pic: user.profile_pic
+        }
+      },
+      {
+        new: true,
+        useFindAndModify: false
+      },
+      (err, user) => {
         if (err) {
-            return res.json({ error: "Problem with image!" });
+          return res.json({ error: "user not updated!" });
         }
-        const { name, phone, address, userInfo } = fields
-        if (!name || !phone || !address || !userInfo) {
-            return res.json({ error: "Provide all info" });
-        }
-
-        let user = req.profile;
-        user = _.extend(user, fields); //allows to access extra fields 
-        if (file.profile_pic) {
-            if (file.profile_pic.size > 3000000) {
-                return res.json({ error: "File size too big!" })
-            }
-
-            user.profile_pic.data = fs.readFileSync(file.profile_pic.filepath) //adding an attribute
-            user.profile_pic.contentType = file.profile_pic.mimetype
-        }
-        User.findOneAndUpdate({ _id: req.profile._id }, {
-            $set: {
-                "name": user.name,
-                "address": user.address,
-                "phone": user.phone,
-                "userInfo": user.userInfo,
-                "profile_pic": user.profile_pic
-            }
-        }, {
-            new: true,
-            useFindAndModify: false
-        }, (err, user) => {
-            if (err) {
-                return res.json({ error: "user not updated!" })
-            }
-            return res.json({ message: "updated successfully!" })
-        })
-    });
-
-}
+        return res.json({ message: "updated successfully!" });
+      }
+    );
+  });
+};
